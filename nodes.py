f98e55f28ad7d3455358aed7538166edd899e67b
LumiBunny <lumi.bunny.gaming@gmail.com>
2024-10-25 22:15:07 -0400

    New Nodes module: Node constructor and registry w/functions.

--- nodes.py
+++ nodes.py
@@ -1,6 +1,7 @@
 import asyncio
 from messages import TextFormatting, Prompting, PostChat, ChatLog
 from chat_completions import Completions
+from node_manager import NodeManager
 from memory import Memory
 
 previous_transcription = ""
@@ -14,31 +15,6 @@ class Node:
     async def process(self, *args):
         if self.handler:
             return await self.handler(*args)
-
-from nodes import Node
-
-class NodeManager:
-    def __init__(self, registry):
-        self.registry = registry
-        self.current_node = None
-
-    def set_initial_node(self, node_name):
-        self.current_node = self.get_node_by_name(node_name)
-
-    def get_node_by_name(self, name):
-        return self.registry.nodes.get(name)  # Updated to use registry's nodes dict directly
-
-    def transition_to_node(self, node_name):
-        new_node = self.get_node_by_name(node_name)
-        if new_node:
-            self.current_node = new_node
-            print(f"Transitioned to node: {self.current_node.name}")
-        else:
-            print(f"Node {node_name} not found!")
-
-    async def process_current_node(self, *args):
-        if self.current_node:
-            return await self.current_node.process(*args)
 
 # nodes.py
 class NodeRegistry:
